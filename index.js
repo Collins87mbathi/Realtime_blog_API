22306075794959ac3248e34a4c975ad6b821e6e4
collins mbathi <perezcoolins@gmail.com>
2022-09-06 13:58:44 +0300

    done

--- index.js
+++ index.js
@@ -18,9 +18,7 @@ const cors = require('cors');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
-app.use(cors({origin:'*', 
-credentials:true,            //access-control-allow-credentials:true
-optionSuccessStatus:200,}));
+app.use(cors({origin:'*'}));
 app.use(fileUpload({    
   useTempFiles: true
 }));
