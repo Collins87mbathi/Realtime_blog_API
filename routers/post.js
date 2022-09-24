23201ff06341b9a87b32cacfc8ad13931341d6c3
collins mbathi <perezcoolins@gmail.com>
2022-09-24 15:40:35 +0300

    fixed

--- routers/post.js
+++ routers/post.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const {createPost,getAllPost,getById,deletePost,updatePost} = require("../controllers/post");
 const {verifyToken,verifyAdmin,verifyUser} = require("../utils/verifyToken");
 
-router.post('/create', createPost);
+router.post('/create',verifyToken, createPost);
 router.get('/all',getAllPost);
 router.put('/:id',verifyToken,verifyUser,updatePost);
 router.get('/:id',getById);
