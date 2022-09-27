6376e0986182c6a75693c43dbe6d7860dce424dc
collins mbathi <perezcoolins@gmail.com>
2022-09-27 11:45:51 +0300

    fixed all errors

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use(helmet());
 //   })
 // )
 
-app.use(cors({ credentials:true, origin:'https://collinsblogs.herokuapp.com'}));
+app.use(cors({ credentials:true, origin:'http://localhost:3000'}));
 
 //client
 // app.use(express.static(path.join(__dirname, "/client/build")));
@@ -65,7 +65,7 @@ db.sequelize.sync()
 
 const io = new Server(http,{ 
   cors: {
-    origin: "https://collinsblogs.herokuapp.com"
+    origin: "http://localhost:3000"
 }
 });
 
